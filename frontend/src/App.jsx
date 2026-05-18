4e76ff535f34e9bf88a8cd793e095e20c43ad4f8
Sambi <sambisingh289@gmail.com>
2026-05-18 18:33:19 -0500

    creted first react page

---
+++ frontend/src/App.jsx
@@ -0,0 +1,20 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from './assets/vite.svg'
+import heroImg from './assets/hero.png'
+import './App.css'
+
+// Two types componenets --> class and 
+const Card = () => {
+  return(<div><h2>Card Componenet</h2></div>)
+}
+const App = () => {
+
+  return(
+    <div className = "card-container">  <h2>Functional Arrow Component</h2>
+    
+</div>
+)
+}
+
+export default App
